993dc2d3bb809082284f53de73bd72bb56497654
Iury Bianchini <iurybianchini@gmail.com>
2024-09-19 14:59:33 +0100

    Corrige nome da função comprimeJavaScripit no gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,6 @@ function compilaSass(){
 exports.images = comprimeImagens;
 exports.default = function() {
     gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
-    gulp.watch('./source/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScripit));
     gulp.watch('./source/images/*', {ignoreInitial: false}, gulp.series(comprimeImagens));
 }
