9937cd12212deb26cabb93ea4314c1b5b60e12db
Iury Bianchini <iurybianchini@gmail.com>
2024-09-19 13:23:47 +0100

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,30 +6,31 @@ const obfuscate = require('gulp-obfuscate');
 const imagemin = require('gulp-imagemin');
 
 function comprimeImagens(){
-    return gulp.src('./source/images/*') //não colocamos extensão pois pode haver varias
+    return gulp.src('./source/images/*') 
     .pipe(imagemin())
     .pipe(gulp.dest('./build/images'))
 }
 
 function comprimeJavaScripit(){
-    return gulp.src('./source/scripts/*.js') //retorno chamando a função gulp.src (passar os arquivos definindo a pasta de origem e todos arquivos)
-    .pipe(uglify()) //pluggin adicionado
-    .pipe(obfuscate()) //obfuscação deve ser inserida antes da parta de destino
-    .pipe(gulp.dest('./build/scripts'))//pasta de destino para arquivos
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/scripts'))
 }
 
 function compilaSass(){
     return gulp.src('./source/styles/main.scss')
-        .pipe(sourcemaps.init()) //executamos o pluguing
+        .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
-        })) //sass alem de copilar deve minificar
-        .pipe(sourcemaps.write('./maps')) //para criar o arquivo do mapa, junto com uma pasta, este caminho ele ja vai considerar o caminho os arquivos css ja estão
-        .pipe(gulp.dest('.build/styles'));
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
 }
 
+exports.images = comprimeImagens;
 exports.default = function() {
     gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
-    gulp.watch('./source/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScripit));
+    gulp.watch('./source/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScript));
     gulp.watch('./source/images/*', {ignoreInitial: false}, gulp.series(comprimeImagens));
 }
